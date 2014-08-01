128b574693f0e21bceffd0b9ec2cb6453e2db06b
unknown <nikul@sys88.BIZTECHC.COM>
2014-08-01 14:25:56 +0530

    Solve issue for Domain locking/unlocking is not working

--- opensrs/domains/provisioning/provModify.php
+++ opensrs/domains/provisioning/provModify.php
@@ -308,7 +308,9 @@ class provModify extends openSRS_base {
 			'protocol' => 'XCP',
 			'action' => 'modify',
 			'object' => 'DOMAIN',
-			'cookie' => $this->_dataObject->data->cookie,
+			/* Commented by BC : RA : 24-6-2014 : To set same XML call as lookupGetDomain */
+            /* 'cookie' => $this->_dataObject->data->cookie, */
+            /* End : To set same XML call as lookupGetDomain */
 			'attributes' => array (
 				'affect_domains' => $this->_dataObject->data->affect_domains,
 				'data' => $this->_dataObject->data->data
