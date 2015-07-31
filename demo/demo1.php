798685151c91afcd57cf4d0268a8d5430308f111
Colin Campbell <ccampbell@tucows.com>
2015-07-31 09:55:39 -0400

    Updated loader to use spyc composer dependancy instead of bundled spyc

--- demo/demo1.php
+++ demo/demo1.php
@@ -1,39 +1,38 @@
-<?
+<?php
+
 $callArray = array (
-	"func" => "premiumDomain",
+	'func' => 'premiumDomain',
 	//"func" => "allinone",
         //"func" => "premiumDomain",
 
-        "data" => array (
-                "domain" => "hockey.com",
+        'data' => array (
+                'domain' => 'hockey.com',
 		// These are optional
-                "selected" => ".com;.net;.org",
-                "alldomains" => ".com;.net;.org" 
-        )
+                'selected' => '.com;.net;.org',
+                'alldomains' => '.com;.net;.org', 
+        ),
 );
 
-require_once ("../opensrs/openSRS_loader.php");
+require_once '../opensrs/openSRS_loader.php';
 
 //JSON
-$callstring = json_encode($callArray);
-$osrsHandler = processOpenSRS ("json", $callstring);
-
+// $callstring = json_encode($callArray);
+// $osrsHandler = processOpenSRS ('json', $callstring);
 
 //YAML
-//$callstring = Spyc::YAMLDump($callArray);
-//$osrsHandler = processOpenSRS ("yaml", $callstring);
+$callstring = Spyc::YAMLDump($callArray);
+$osrsHandler = processOpenSRS ("yaml", $callstring);
 
 //$callstring = XML($callArray);
 //$osrsHandler = processOpenSRS ("xml", $callstring);
 
-echo (" In: ". $callstring ."<br>");
+echo (' In: '.$callstring.'<br>');
 //echo ("Out Result Full Raw: ". $osrsHandler->resultFullRaw ."<br>");
 //echo ("Out Results Formatted: ". $osrsHandler->resultFormatted ."<br>");
 
 //Array
 $variable = var_dump($osrsHandler->resultRaw, true);
 $variable = str_replace ("\n", "<br>\n",  $variable);
-echo ("Out Results Formatted: ". $variable);
-
+echo ('Out Results Formatted: '.$variable);
 
 ?>
--- opensrs/openSRS_loader.php
+++ opensrs/openSRS_loader.php
@@ -1,122 +1,146 @@
 <?php
 
-require_once dirname(__FILE__) . '/openSRS_config.php';
+require dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__).'/openSRS_config.php';
 
 final class Autoloader
 {
-  static public function load( $classname )
-  {
-    $lib_paths = array(OPENSRSURI, OPENSRSMAIL, OPENSRSOMA, OPENSRSFASTLOOKUP, OPENSRSTRUST);
-    
-    foreach ($lib_paths as &$path) {
-    	$classfile = $path . DS . $classname . ".php";
-    	if(file_exists( $classfile )) {
-      	require_once $classfile;
-    	}
-  	}
-  } 
-  static public function load_domains( $classname )
-  {
-    $iterator = new DirectoryIterator(OPENSRSDOMAINS);
-    foreach($iterator as $dir){
-    	if($dir->isDot()) continue;
-      $classfile = OPENSRSDOMAINS . DS . $dir . DS . $classname . ".php";
-    	if(file_exists( $classfile )) {
-      	require_once $classfile;
-    	}
+    public static function load($classname)
+    {
+        $lib_paths = array(OPENSRSURI, OPENSRSMAIL, OPENSRSOMA, OPENSRSFASTLOOKUP, OPENSRSTRUST);
+
+        foreach ($lib_paths as &$path) {
+            $classfile = $path.DS.$classname.'.php';
+            if (file_exists($classfile)) {
+                require_once $classfile;
+            }
+        }
     }
-  }
+    public static function load_domains($classname)
+    {
+        $iterator = new DirectoryIterator(OPENSRSDOMAINS);
+        foreach ($iterator as $dir) {
+            if ($dir->isDot()) {
+                continue;
+            }
+            $classfile = OPENSRSDOMAINS.DS.$dir.DS.$classname.'.php';
+            if (file_exists($classfile)) {
+                require_once $classfile;
+            }
+        }
+    }
 }
 
-spl_autoload_register( array( 'Autoloader', 'load') );
-spl_autoload_register( array( 'Autoloader', 'load_domains') );
+spl_autoload_register(array('Autoloader', 'load'));
+spl_autoload_register(array('Autoloader', 'load_domains'));
 
 /**
-* Method to convert Array -> Object -> Array
-*
-* @param  hash  $data  Containing array object
-* 
-* @return   stdClass Object $object   Containing stdClass object
-* @since    3.4
-*/
-
-function array2object($data) {
-  if(!is_array($data)) return $data;
-  $object = new stdClass();
-  
-  foreach ($data as $name=>$value) {
-    if (isset($name)) {
-      $name = strtolower(trim($name));
-      $object->$name = array2object($value); 
+ * Method to convert Array -> Object -> Array.
+ *
+ * @param hash $data Containing array object
+ * 
+ * @return stdClass Object $object   Containing stdClass object
+ *
+ * @since    3.4
+ */
+function array2object($data)
+{
+    if (!is_array($data)) {
+        return $data;
     }
-  }
-  return $object;
+    $object = new stdClass();
+
+    foreach ($data as $name => $value) {
+        if (isset($name)) {
+            $name = strtolower(trim($name));
+            $object->$name = array2object($value);
+        }
+    }
+
+    return $object;
 }
 
-function object2array($data){
-   if(!is_object($data) && !is_array($data)) return $data;
-   if(is_object($data)) $data = get_object_vars($data);
-   return array_map('object2array', $data);
+function object2array($data)
+{
+    if (!is_object($data) && !is_array($data)) {
+        return $data;
+    }
+    if (is_object($data)) {
+        $data = get_object_vars($data);
+    }
+
+    return array_map('object2array', $data);
 }
 
 // Call parsers and functions of openSRS
-function processOpenSRS ($type="", $data="") {
-
+function processOpenSRS($type = '', $data = '')
+{
     if (empty($data)) {
-        trigger_error("OSRS Error - No data found.");
-        return null;
-    }
-    else {
+        trigger_error('OSRS Error - No data found.');
+
+        return;
+    } else {
         $dataArray = array();
-        switch(strtolower($type)) {
-            case "array":
+        switch (strtolower($type)) {
+            case 'array':
                 $dataArray = $data;
                 break;
-            case "json":
-                $json = str_replace("\\\"", "\"", $data);   //  Replace  \"  with " for JSON that comes from Javascript
+            case 'json':
+                $json = str_replace('\\"', '"', $data);   //  Replace  \"  with " for JSON that comes from Javascript
                 $dataArray = json_decode($json, true);
                 break;
-            case "yaml":
+            case 'yaml':
                 $dataArray = Spyc::YAMLLoad($data);
                 break;
             default:
                 $dataArray = $data;
         }
-		    // Convert associative array to object
+            // Convert associative array to object
         $dataObject = array2object($dataArray);
         $classCall = null;
-        if (class_exists($dataObject->func)){
+        if (class_exists($dataObject->func)) {
             $classCall = new $dataObject->func($type, $dataObject);
-        } 
-        else {
+        } else {
             trigger_error("OSRS Error - Unable to find the function $dataObject->func.  Either the function is misspelled or there are incorrect file paths set in openSRS_config.php.");
         }
+
         return $classCall;
     }
 }
 
-function convertArray2Formatted ($type="", $data="") {
-  $resultString = "";
-  if ($type == "json") $resultString = json_encode($data);
-  if ($type == "yaml") $resultString = Spyc::YAMLDump($data);
-  return $resultString;
+function convertArray2Formatted($type = '', $data = '')
+{
+    $resultString = '';
+    if ($type == 'json') {
+        $resultString = json_encode($data);
+    }
+    if ($type == 'yaml') {
+        $resultString = Spyc::YAMLDump($data);
+    }
+
+    return $resultString;
 }
 
-function convertFormatted2array ($type="", $data="") {
-	$resultArray = "";
-	if ($type == "json") $resultArray = json_decode($data, true);
-	if ($type == "yaml") $resultArray = Spyc::YAMLLoad($data);;
-	return $resultArray;
+function convertFormatted2array($type = '', $data = '')
+{
+    $resultArray = '';
+    if ($type == 'json') {
+        $resultArray = json_decode($data, true);
+    }
+    if ($type == 'yaml') {
+        $resultArray = Spyc::YAMLLoad($data);
+    }
+
+    return $resultArray;
 }
 
 function array_filter_recursive($input)
 {
-  foreach ($input as &$value)
-  {
-    if (is_array($value))
-    {
-      $value = array_filter_recursive($value);
+    foreach ($input as &$value) {
+        if (is_array($value)) {
+            $value = array_filter_recursive($value);
+        }
     }
-  } 
-  return array_filter($input);
+
+    return array_filter($input);
 }
