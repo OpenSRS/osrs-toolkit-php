ad935ff517929803305a828901a6dcce0ada993e
Colin Campbell <ccampbell@tucows.com>
2015-08-07 13:26:30 -0400

    Update demo2 to use the reseller username from the config file

--- demo/demo2.php
+++ demo/demo2.php
@@ -1,17 +1,18 @@
-<?
+<?php
+
 $callArray = array (
-        "func" => "subresGet",
-        "data" => array (
-                "username" => "clamsub10"
-        )
+        'func' => 'subresGet',
+        'data' => array (
+                'username' => OSRS_USERNAME,
+        ),
 );
 
-require_once("../opensrs/openSRS_loader.php");
+require_once '../opensrs/openSRS_loader.php';
 
 $callstring = json_encode($callArray);
 
-$osrsHandler = processOpenSRS ("json", $callstring);
+$osrsHandler = processOpenSRS ('json', $callstring);
 
-echo (" In: ". $callstring ."<br>");
-echo ("Out: ". $osrsHandler->resultFormatted);
+echo (' In: '.$callstring.'<br>');
+echo ('Out: '.$osrsHandler->resultFormatted);
 ?>
