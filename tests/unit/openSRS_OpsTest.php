5375dd17eee989fc019543376e78c6ad98af8011
Colin Campbell <ccampbell@tucows.com>
2015-08-10 15:05:19 -0400

    Add test for Ops is associative

--- tests/unit/openSRS_OpsTest.php
+++ tests/unit/openSRS_OpsTest.php
@@ -21,4 +21,19 @@ class openSRS_OpsTest extends PHPUnit_Framework_TestCase
         // should be utf8
         $this->assertTrue(mb_check_encoding($result, 'UTF-8'));
     }
+
+    /**
+     * Should return true for associative, and false for plain
+     * 
+     * @return void
+     */
+    public function testIsAssoc()
+    {
+        $ops = new openSRS_ops;
+        $no = array(1, 2, 3, 4);
+        $yes = array('this' => 'is', 'associative');
+
+        $this->assertfalse($ops->_is_assoc($no));
+        $this->assertTrue($ops->_is_assoc($yes));
+    }
 }
