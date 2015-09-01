7554d7d511223624ec757776b993f83f2f2d0db4
Colin Campbell <colin@elusivelabs.com>
2015-09-01 13:56:03 -0400

    Refactor LookupDomain

--- opensrs/domains/lookup/LookupDomain.php
+++ opensrs/domains/lookup/LookupDomain.php
@@ -8,17 +8,17 @@ use OpenSRS\Exception;
 class LookupDomain extends Base
 {
     private $_domain = '';
-    private $_dataObject;
     private $_formatHolder = '';
     public $resultFullRaw;
     public $resultRaw;
     public $resultFullFormatted;
     public $resultFormatted;
+    protected $defaultTlds = array('.com','.net','.org', '.ca');
 
     public function __construct($formatString, $dataObject)
     {
         parent::__construct();
-        $this->_dataObject = $dataObject;
+        $this->dataObject = $dataObject;
         $this->_formatHolder = $formatString;
         $this->_validateObject();
     }
@@ -31,46 +31,17 @@ class LookupDomain extends Base
     // Validate the object
     private function _validateObject()
     {
-        $allPassed = true;
         $domain = '';
-        $arraSelected = array();
-        $arraAll = array();
-        $arraCall = array();
 
-        if (isset($this->_dataObject->data->domain)) {
-            // Grab domain name
-            $domain = $this->_dataObject->data->domain;
-        } else {
-            throw new Exception('oSRS Error - Search domain strinng not defined.');
-            // trigger_error('oSRS Error - Search domain strinng not defined.', E_USER_WARNING);
-            $allPassed = false;
+        // search domain must be definded
+        if (!isset($this->dataObject->data->domain)) {
+            throw new Exception('oSRS Error - Search domain string not defined.');
         }
 
-        if (isset($this->_dataObject->data->selected) && $this->_dataObject->data->selected != '') {
-            $arraSelected = explode(';', $this->_dataObject->data->selected);
-        }
-        if (isset($this->_dataObject->data->defaulttld) && $this->_dataObject->data->defaulttld != '') {
-            $arraAll = explode(';', $this->_dataObject->data->defaulttld);
-        }
-
-        if (count($arraSelected) == 0) {
-            if (count($arraAll) == 0) {
-                $arraCall = array('.com','.net','.org', '.ca');
-            } else {
-                $arraCall = $arraAll;
-            }
-        } else {
-            $arraCall = $arraSelected;
-        }
+        $tlds = $this->getConfiguredTlds();
 
         // Call function
-        if ($allPassed) {
-            $resObject = $this->_domainTLD($domain, $arraCall);
-        } else {
-            
-            throw new Exception('oSRS Error - Incorrect call.');
-            // trigger_error('oSRS Error - Incorrect call.', E_USER_WARNING);
-        }
+        $resObject = $this->_domainTLD($domain, $tlds);
     }
 
     // Selected / all TLD options
@@ -93,21 +64,13 @@ class LookupDomain extends Base
             ),
         );
 
-        if (isset($this->_dataObject->data->maximum) && $this->_dataObject->data->maximum != '') {
-            $cmd['attributes']['service_override']['lookup']['maximum'] = $this->_dataObject->data->maximum;
+        if (isset($this->dataObject->data->maximum) && $this->dataObject->data->maximum != '') {
+            $cmd['attributes']['service_override']['lookup']['maximum'] = $this->dataObject->data->maximum;
         }
 
-//		print_r ($cmd);
-//		echo ("\n\n\n");
         $xmlCMD = $this->_opsHandler->encode($cmd);                    // Flip Array to XML
-//		echo ($xmlCMD);
-//		echo ("\n\n\n");
         $XMLresult = $this->send_cmd($xmlCMD);                        // Send XML
-//		echo ($XMLresult);
-//		echo ("\n\n\n");
         $arrayResult = $this->_opsHandler->decode($XMLresult);        // FLip XML to Array
-//		print_r ($arrayResult);
-//		echo ("\n\n\n");
 
         // Results
         $this->resultFullRaw = $arrayResult;
