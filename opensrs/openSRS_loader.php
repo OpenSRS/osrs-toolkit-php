9438f71751c6961ff9833e1b43fff88bb8924bc4
Kris Atkinson <kris@atkinson.me>
2015-09-02 15:26:59 -0400

    update old openSRS_loader.php file to use new autoloader without breaking backward compatibility

--- opensrs/openSRS_loader.php
+++ opensrs/openSRS_loader.php
@@ -5,37 +5,6 @@ require_once dirname(__FILE__).'/openSRS_config.php';
 
 use OpenSRS\Request;
 
-// final class Autoloader
-// {
-//     public static function load($classname)
-//     {
-//         $lib_paths = array(OPENSRSURI, OPENSRSMAIL, OPENSRSOMA, OPENSRSFASTLOOKUP, OPENSRSTRUST);
-
-//         foreach ($lib_paths as &$path) {
-//             $classfile = $path.DS.$classname.'.php';
-//             if (file_exists($classfile)) {
-//                 require_once $classfile;
-//             }
-//         }
-//     }
-//     public static function load_domains($classname)
-//     {
-//         $iterator = new DirectoryIterator(OPENSRSDOMAINS);
-//         foreach ($iterator as $dir) {
-//             if( $dir->isDot() ) {
-//                 continue;
-//             }
-//             $classfile = OPENSRSDOMAINS . DS . $dir . DS . $classname . '.php';
-//             if( file_exists($classfile) ) {
-//                 require_once $classfile;
-//             }
-//         }
-//     }
-// }
-
-// spl_autoload_register(array('Autoloader', 'load'));
-// spl_autoload_register(array('Autoloader', 'load_domains'));
-
 /**
  * Method to convert Array -> Object -> Array.
  *
@@ -85,37 +54,6 @@ function processOpenSRS($type = '', $data = '')
     catch(\OpenSRS\Exception $e) {
         return ($e->getMessage());
     }
-
-    // if (empty($data)) {
-    //     trigger_error('OSRS Error - No data found.');
-    //     return;
-    // } else {
-    //     $dataArray = array();
-    //     switch (strtolower($type)) {
-    //         case 'array':
-    //             $dataArray = $data;
-    //             break;
-    //         case 'json':
-    //             $json = str_replace('\\"', '"', $data);   //  Replace  \"  with " for JSON that comes from Javascript
-    //             $dataArray = json_decode($json, true);
-    //             break;
-    //         case 'yaml':
-    //             $dataArray = Spyc::YAMLLoad($data);
-    //             break;
-    //         default:
-    //             $dataArray = $data;
-    //     }
-    //         // Convert associative array to object
-    //     $dataObject = array2object($dataArray);
-    //     $classCall = null;
-    //     if (class_exists($dataObject->func)) {
-    //         $classCall = new $dataObject->func($type, $dataObject);
-    //     } else {
-    //         trigger_error("OSRS Error - Unable to find the function $dataObject->func.  Either the function is misspelled or there are incorrect file paths set in openSRS_config.php.");
-    //     }
-
-    //     return $classCall;
-    // }
 }
 
 function convertArray2Formatted($type = '', $data = '')
