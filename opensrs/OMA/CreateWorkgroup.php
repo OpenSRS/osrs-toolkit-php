354e249012925d8f21bc0c071a57b9f9eec20217
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:07:58 -0400

    Update OMA/CreateWorkgroup to namespace

--- opensrs/OMA/CreateWorkgroup.php
+++ opensrs/OMA/CreateWorkgroup.php
@@ -1,23 +1,28 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: create_workgroup
 // Create a workgroup in a domain 
 
-class CreateWorkgroup {
+class CreateWorkgroup
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('create_workgroup', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)){
-    		return OMA::send_cmd("create_workgroup", $data);
-    	}
-	}
-
-	// Valdation rule here
-    public static function validate($data) {
-    	if(empty($data["workgroup"]) || empty($data["domain"])){
-			trigger_error("oSRS Error - Workgroup/Domain required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    // Valdation rule here
+    public static function validate($data)
+    {
+        if (empty($data['workgroup']) || empty($data['domain'])) {
+            trigger_error("oSRS Error - Workgroup/Domain required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
