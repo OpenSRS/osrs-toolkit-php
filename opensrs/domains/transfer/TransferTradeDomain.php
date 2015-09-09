45d2a767c4bcc464c40634b7e0b4cd5910dd9f6d
Kris Atkinson <kris@atkinson.me>
2015-09-09 14:47:22 -0400

    add TransferSendPassword and TransferTradeDomain namespaced classes, unit tests

--- opensrs/domains/transfer/TransferTradeDomain.php
+++ opensrs/domains/transfer/TransferTradeDomain.php
@@ -82,9 +82,12 @@ class TransferTradeDomain extends Base {
 			)
 		);
 
-		$xmlCMD = $this->_opsHandler->encode( $cmd );					// Flip Array to XML
-		$XMLresult = $this->send_cmd( $xmlCMD );						// Send XML
-		$arrayResult = $this->_opsHandler->decode( $XMLresult );		// Flip XML to Array
+        // Flip Array to XML
+        $xmlCMD = $this->_opsHandler->encode( $cmd );
+        // Send XML
+        $XMLresult = $this->send_cmd( $xmlCMD );
+        // Flip XML to Array
+        $arrayResult = $this->_opsHandler->decode( $XMLresult );
 
 		// Results
 		$this->resultFullRaw = $arrayResult;
