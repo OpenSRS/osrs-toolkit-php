41f20108cdf3d4c58aaa1bb5e7acb49c2d84a435
Colin Campbell <colin@elusivelabs.com>
2015-09-09 09:55:59 -0400

    Update OMA/Authenticate to namespace

--- opensrs/OMA/Authenticate.php
+++ opensrs/OMA/Authenticate.php
@@ -1,18 +1,23 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: authenticate
 // Authenticate a user's credentials. 
 
-class Authenticate {
+class Authenticate
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('authenticate', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)){
-    		return OMA::send_cmd("authenticate", $data);
-    	}
-	}
-
-    public static function validate($data) {
-    	return true;
-  	}
+    public static function validate($data)
+    {
+        return true;
+    }
 }
-?>
