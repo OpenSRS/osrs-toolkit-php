8b1019a6e16c2f37d0fd1a81a54c06212d463055
Colin Campbell <colin@elusivelabs.com>
2015-09-09 11:09:00 -0400

    Update OMA/MigrationAdd to namespace

--- opensrs/OMA/MigrationAdd.php
+++ opensrs/OMA/MigrationAdd.php
@@ -1,19 +1,24 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: migration_add
 // Create a bulk migration job, to copy email from many remote accounts to many local accounts. 	
 
-class MigrationAdd {
+class MigrationAdd
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('migration_add', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)){
-    		return OMA::send_cmd("migration_add", $data);
-    	}
-	}
-
-	// Valdation rule here
-    public static function validate($data) {
-  		return true;
-	}
+    // Valdation rule here
+    public static function validate($data)
+    {
+        return true;
+    }
 }
-?>
