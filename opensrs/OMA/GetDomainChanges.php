9da52cbe7c9d0dca6bf5e7bcc95ca4a4734a715d
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:22:45 -0400

    Update OMA/GetDomainChanges to namespace

--- opensrs/OMA/GetDomainChanges.php
+++ opensrs/OMA/GetDomainChanges.php
@@ -1,22 +1,27 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: get_domain_changes
 // Retrieve a summary of changes to a domain 
 
-class GetDomainChanges {
+class GetDomainChanges
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('get_domain_changes', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)) {
-    		return OMA::send_cmd("get_domain_changes", $data);
-    	}
-	}
-
-    public static function validate($data) {
-    	if(empty($data['domain'])){
-			trigger_error("oSRS Error - Domain required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    public static function validate($data)
+    {
+        if (empty($data['domain'])) {
+            trigger_error("oSRS Error - Domain required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
