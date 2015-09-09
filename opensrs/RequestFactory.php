8979f98704ac9ffc7cc422a84228cde428163473
Colin Campbell <colin@elusivelabs.com>
2015-09-09 16:09:06 -0400

    Update mail/GetCompanyDomains to namespace

--- opensrs/RequestFactory.php
+++ opensrs/RequestFactory.php
@@ -455,6 +455,10 @@ class RequestFactory
                 return new \OpenSRS\mail\DeleteMailbox( $type, $dataObject );
                 break;
 
+            case 'mailGetCompanyDomains':
+                return new \OpenSRS\mail\GetCompanyDomains( $type, $dataObject );
+                break;
+
 
             default:
                 throw new Exception( "OSRS Error - $func is unsupported." );
