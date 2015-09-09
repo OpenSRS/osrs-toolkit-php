768fc929471be42757de8409259c72b3487eaceb
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:34:24 -0400

    Update OMA/RestoreDeleteContacts to namespace

--- opensrs/OMA/RestoreDeletedContacts.php
+++ opensrs/OMA/RestoreDeletedContacts.php
@@ -1,22 +1,27 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: restore_deleted_contact
 // Restore deleted contacts for a user 
 
-class RestoreDeletedContacts {
+class RestoreDeletedContacts
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('restore_deleted_contacts', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)) {
-    		return OMA::send_cmd("restore_deleted_contacts", $data);
-    	}
-	}
-
-    public static function validate($data) {
-    	if(empty($data['user']) || empty($data['ids'])){
-			trigger_error("oSRS Error - User/IDs required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    public static function validate($data)
+    {
+        if (empty($data['user']) || empty($data['ids'])) {
+            trigger_error("oSRS Error - User/IDs required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
