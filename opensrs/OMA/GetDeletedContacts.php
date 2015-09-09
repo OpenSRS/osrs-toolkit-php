45d46d4a8bc6bf2e071508d1e124116bcef5fe32
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:19:32 -0400

    Update OMA/GetDeletedContacts to namespace

--- opensrs/OMA/GetDeletedContacts.php
+++ opensrs/OMA/GetDeletedContacts.php
@@ -1,22 +1,27 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: get_deleted_contacts
 // Retrieves a list of deleted restorable contacts from a user's wembail address book. 
 
-class GetDeletedContacts {
+class GetDeletedContacts
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('get_deleted_contacts', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)) {
-    		return OMA::send_cmd("get_deleted_contacts", $data);
-    	}
-	}
-
-    public static function validate($data) {
-    	if(empty($data['user'])){
-			trigger_error("oSRS Error - User required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    public static function validate($data)
+    {
+        if (empty($data['user'])) {
+            trigger_error("oSRS Error - User required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
