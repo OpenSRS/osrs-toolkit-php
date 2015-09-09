e5ea5e639075663c4b4e74692cad2efe122eb0ac
Colin Campbell <colin@elusivelabs.com>
2015-09-09 16:11:21 -0400

    Update mail/GetDomainAllowList to namespace

--- opensrs/RequestFactory.php
+++ opensrs/RequestFactory.php
@@ -459,6 +459,10 @@ class RequestFactory
                 return new \OpenSRS\mail\GetCompanyDomains( $type, $dataObject );
                 break;
 
+            case 'mailGetDomainAllowList':
+                return new \OpenSRS\mail\GetDomainAllowList( $type, $dataObject );
+                break;
+
 
             default:
                 throw new Exception( "OSRS Error - $func is unsupported." );
