33e93d09f0de8a153a6cc025628691f0f2d3d494
Colin Campbell <colin@elusivelabs.com>
2015-09-09 15:33:05 -0400

    Update mail/DeleteDomainAlias to namespace

--- opensrs/RequestFactory.php
+++ opensrs/RequestFactory.php
@@ -439,6 +439,11 @@ class RequestFactory
                 return new \OpenSRS\mail\CreateMailbox( $type, $dataObject );
                 break;
 
+            case 'mailDeleteDomainAlias':
+                return new \OpenSRS\mail\DeleteDomainAlias( $type, $dataObject );
+                break;
+
+
             default:
                 throw new Exception( "OSRS Error - $func is unsupported." );
                 break;
