16fe216fdff934dd00f65afb2d405572bd0e5cfd
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:20:35 -0400

    Update OMA/GetDeletedMessages to namespace

--- opensrs/OMA/GetDeletedMessages.php
+++ opensrs/OMA/GetDeletedMessages.php
@@ -1,23 +1,28 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: get_deleted_messages
 // Retrieve a list of recoverable deleted emails belonging to a user  	
 
-class GetDeletedMessages {
+class GetDeletedMessages
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('get_deleted_messages', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)){
-    		return OMA::send_cmd("get_deleted_messages", $data);
-    	}
-	}
-
-	// Valdation rule here
-    public static function validate($data) {
-    	if(empty($data["user"])){
-			trigger_error("oSRS Error - User required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    // Valdation rule here
+    public static function validate($data)
+    {
+        if (empty($data['user'])) {
+            trigger_error("oSRS Error - User required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
