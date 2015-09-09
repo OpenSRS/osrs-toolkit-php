6b2a1f6e7e0611daedba47e866c89834c920b9c6
Colin Campbell <colin@elusivelabs.com>
2015-09-09 10:33:32 -0400

    Update OMA/RenameUser to namespace

--- opensrs/OMA/RenameUser.php
+++ opensrs/OMA/RenameUser.php
@@ -1,22 +1,27 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: rename_user
 // Rename a user 
 
-class RenameUser {
+class RenameUser
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('rename_user', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)) {
-    		return OMA::send_cmd("rename_user", $data);
-    	}
-	}
-
-    public static function validate($data) {
-    	if(empty($data['user']) || empty($data['new_name'])){
-			trigger_error("oSRS Error - User/IDs required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    public static function validate($data)
+    {
+        if (empty($data['user']) || empty($data['new_name'])) {
+            trigger_error("oSRS Error - User/IDs required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
