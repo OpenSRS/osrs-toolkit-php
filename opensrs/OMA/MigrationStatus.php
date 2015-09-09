e1959f11df6929ac07cd1f46aa2130468d24e9c9
Colin Campbell <colin@elusivelabs.com>
2015-09-09 11:10:38 -0400

    Update OMA/MigrationStatus to namespace

--- opensrs/OMA/MigrationStatus.php
+++ opensrs/OMA/MigrationStatus.php
@@ -1,22 +1,27 @@
 <?php
 
+namespace OpenSRS\OMA;
+
+use OpenSRS\OMA;
+
 // command: migration_status
 // Get detailed information about the progress and results of a migration job.  	
-class MigrationStatus {
+class MigrationStatus
+{
+    public static function call($data)
+    {
+        if (self::validate($data)) {
+            return OMA::send_cmd('migration_status', $data);
+        }
+    }
 
-	public static function call($data) {
-		if (self::validate($data)){
-    		return OMA::send_cmd("migration_status", $data);
-    	}
-	}
-
-	// Valdation rule here
-    public static function validate($data) {
-    	if(empty($data['job'])){
-			trigger_error("oSRS Error - Job required\n", E_USER_WARNING);	
-		} else {
-			return true;
-		}
-  	}
+    // Valdation rule here
+    public static function validate($data)
+    {
+        if (empty($data['job'])) {
+            trigger_error("oSRS Error - Job required\n", E_USER_WARNING);
+        } else {
+            return true;
+        }
+    }
 }
-?>
