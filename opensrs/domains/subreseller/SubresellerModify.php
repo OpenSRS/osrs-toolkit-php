d8ee451de4dcc3ccde914c5ce0af6896359757bd
Kris Atkinson <kris@atkinson.me>
2015-09-09 09:36:24 -0400

    add SubresellerModify class and unit test

--- opensrs/domains/subreseller/SubresellerModify.php
+++ opensrs/domains/subreseller/SubresellerModify.php
@@ -10,7 +10,7 @@ use OpenSRS\Exception;
  *  data -
  */
 
-class SubresellerCreate extends Base {
+class SubresellerModify extends Base {
 	private $_dataObject;
 	private $_formatHolder = "";
 	public $resultFullRaw;
@@ -30,20 +30,33 @@ class SubresellerCreate extends Base {
 	}
 
 	// Validate the object
-	private function _validateObject() {
-		$reqPers = array( "first_name", "last_name", "org_name", "address1", "city", "state", "country", "postal_code", "phone", "email", "lang_pref" );
-		for( $i = 0; $i < count($reqPers); $i++ ) {
-			if( $this->_dataObject->personal->$reqPers[$i] == "" ) {
+	private function _validateObject(){
+		$reqPers = array(
+			"first_name", "last_name", "org_name", "address1",
+			"city", "state", "country", "postal_code",
+			"phone", "email", "lang_pref"
+			);
+
+		for( $i = 0; $i < count($reqPers); $i++ ){
+			if(
+				!isset($this->_dataObject->personal->$reqPers[$i]) ||
+				$this->_dataObject->personal->$reqPers[$i] == ""
+			) {
 				throw new Exception( "oSRS Error - ". $reqPers[$i] ." is not defined." );
 			}
 		}
 
-		$reqData = array( "ccp_enabled", "low_balance_email", "password", "pricing_plan", "status", "system_status_email", "username" );
-		for( $i = 0; $i < count($reqData); $i++ ) {
+		$reqData = array(
+			"ccp_enabled", "low_balance_email", "password",
+			"pricing_plan", "status", "system_status_email",
+			"username"
+			);
+
+		for( $i = 0; $i < count($reqData); $i++ ){
 			if(
 				!isset($this->_dataObject->data->$reqData[$i]) ||
 				$this->_dataObject->data->$reqData[$i] == ""
-				) {
+			) {
 				throw new Exception( "oSRS Error - ". $reqData[$i] ." is not defined." );
 			}
 		}
@@ -53,18 +66,18 @@ class SubresellerCreate extends Base {
 	}
 
 	// Post validation functions
-	private function _processRequest() {
+	private function _processRequest(){
 		$cmd = array(
 			'protocol' => 'XCP',
-			'action' => 'CREATE',
+			'action' => 'MODIFY',
 			'object' => 'SUBRESELLER',
 			'attributes' => array(
 				'ccp_enabled' => $this->_dataObject->data->ccp_enabled,
 				'contact_set' => array(
-					'owner' => $this->_createUserData( 'personal' ),
-					'admin' => $this->_createUserData( 'admin' ),
-					'billing' => $this->_createUserData( 'billing' ),
-					'tech' => $this->_createUserData( 'tech' )
+					'owner' => $this->_createUserData(),
+					'admin' => $this->_createUserData(),
+					'billing' => $this->_createUserData(),
+					'tech' => $this->_createUserData()
 				),
 				'low_balance_email' => $this->_dataObject->data->low_balance_email,
 				'password' => $this->_dataObject->data->password,
@@ -77,24 +90,31 @@ class SubresellerCreate extends Base {
 
 		// Command optional values
 		if(
-			isset( $this->_dataObject->data->payment_email ) &&
+			isSet( $this->_dataObject->data->payment_email ) &&
 			$this->_dataObject->data->payment_email != ""
 		) {
 			$cmd['attributes']['payment_email'] = $this->_dataObject->data->payment_email;
 		}
 		if(
-			isset( $this->_dataObject->data->url ) &&
+			isSet( $this->_dataObject->data->url ) &&
 			$this->_dataObject->data->url != ""
 		) {
 			$cmd['attributes']['url'] = $this->_dataObject->data->url;
 		}
 		if(
-			isset( $this->_dataObject->data->nameservers ) &&
+			isSet( $this->_dataObject->data->storefront_rwi ) &&
+			$this->_dataObject->data->storefront_rwi != ""
+		) {
+			$cmd['attributes']['storefront_rwi'] = $this->_dataObject->data->storefront_rwi;
+		}
+		if(
+			isSet( $this->_dataObject->data->nameservers ) &&
 			$this->_dataObject->data->nameservers != ""
 		) {
 			// 'fqdn1' => 'parking1.mdnsservice.com'
 			$tmpArray = explode( ",", $this->_dataObject->data->nameservers );
-			for( $i=0; $i<count($tmpArray); $i++ ) {
+
+			for( $i=0; $i<count($tmpArray); $i++ ){
 				$cmd['attributes']['nameservers']['fqdn'. ( $i+1 )] = $tmpArray[$i];
 			}
 		}
@@ -108,36 +128,29 @@ class SubresellerCreate extends Base {
 
 		// Results
 		$this->resultFullRaw = $arrayResult;
-        if( isset($arrayResult['attributes'] )) {
-            $this->resultRaw = $arrayResult['attributes'];
-        } else {
-			$this->resultRaw = $arrayResult;
-		}
+		$this->resultRaw = $arrayResult;
 		$this->resultFullFormatted = $this->convertArray2Formatted( $this->_formatHolder, $this->resultFullRaw );
 		$this->resultFormatted = $this->convertArray2Formatted( $this->_formatHolder, $this->resultRaw );
 	}
 
-    private function _createUserData( $contact_type = 'personal' ) {
-    	if( !isset( $this->_dataObject->$contact_type ) ) {
-    		$contact_type = 'personal';
-    		// throw new Exception( "Contact type '$contact_type' is not set");
-    	}
+
+	private function _createUserData(){
 		$userArray = array(
-			"first_name" => $this->_dataObject->$contact_type->first_name,
-			"last_name" => $this->_dataObject->$contact_type->last_name,
-			"org_name" => $this->_dataObject->$contact_type->org_name,
-			"address1" => $this->_dataObject->$contact_type->address1,
-			"address2" => $this->_dataObject->$contact_type->address2,
-			"address3" => $this->_dataObject->$contact_type->address3,
-			"city" => $this->_dataObject->$contact_type->city,
-			"state" => $this->_dataObject->$contact_type->state,
-			"postal_code" => $this->_dataObject->$contact_type->postal_code,
-			"country" => $this->_dataObject->$contact_type->country,
-			"phone" => $this->_dataObject->$contact_type->phone,
-			"fax" => $this->_dataObject->$contact_type->fax,
-			"email" => $this->_dataObject->$contact_type->email,
-			"url" => $this->_dataObject->$contact_type->url,
-			"lang_pref" => $this->_dataObject->$contact_type->lang_pref
+			"first_name" => $this->_dataObject->personal->first_name,
+			"last_name" => $this->_dataObject->personal->last_name,
+			"org_name" => $this->_dataObject->personal->org_name,
+			"address1" => $this->_dataObject->personal->address1,
+			"address2" => $this->_dataObject->personal->address2,
+			"address3" => $this->_dataObject->personal->address3,
+			"city" => $this->_dataObject->personal->city,
+			"state" => $this->_dataObject->personal->state,
+			"postal_code" => $this->_dataObject->personal->postal_code,
+			"country" => $this->_dataObject->personal->country,
+			"phone" => $this->_dataObject->personal->phone,
+			"fax" => $this->_dataObject->personal->fax,
+			"email" => $this->_dataObject->personal->email,
+			"url" => $this->_dataObject->personal->url,
+			"lang_pref" => $this->_dataObject->personal->lang_pref
 		);
 		return $userArray;
 	}
