3bbd25fc86e4fbafacfc8ed85de14de88b84e57e
Kris Atkinson <kris@atkinson.me>
2015-09-10 10:12:35 -0400

    clean up unit DNS unit tests

--- tests/unit/opensrs/domains/bulkchange/BulkSubmitTest.php
+++ tests/unit/opensrs/domains/bulkchange/BulkSubmitTest.php
@@ -20,7 +20,7 @@ class BulkSubmitTest extends PHPUnit_Framework_TestCase
 		'push_domains' => 'Push_domains',
 		'whois_privacy' => 'Whois_privacy'
 		);
-    protected $validSubmission = '{"func":"bulkSubmit","personal":{"first_name":"Claire","last_name":"Lam","org_name":"Tucows","address1":"96 Mowat Avenue","address2":"","address3":"","city":"Toronto","state":"ON","postal_code":"M6K 3M1","country":"CA","phone":"416-535-0123 x1386","fax":"","email":"clam@tucows.com","url":"http:\/\/www.tucows.com","lang_pref":"EN"},"data":{"registrant_ip":"","affiliate_id":"","reg_username":"clam","reg_domain":"","reg_password":"abc123","custom_tech_contact":"","handle":"","domain_list":"","change_items":"phptest.com","change_type":"availability_check","op_type":"export","contact_email":"clam@tucows.com"}}';
+    protected $validSubmission = '{"personal":{"first_name":"Claire","last_name":"Lam","org_name":"Tucows","address1":"96 Mowat Avenue","address2":"","address3":"","city":"Toronto","state":"ON","postal_code":"M6K 3M1","country":"CA","phone":"416-535-0123 x1386","fax":"","email":"clam@tucows.com","url":"http:\/\/www.tucows.com","lang_pref":"EN"},"data":{"registrant_ip":"","affiliate_id":"","reg_username":"clam","reg_domain":"","reg_password":"abc123","custom_tech_contact":"","handle":"","domain_list":"","change_items":"phptest.com","change_type":"availability_check","op_type":"export","contact_email":"clam@tucows.com"}}';
 
     /**
      * Valid submission should complete with no
