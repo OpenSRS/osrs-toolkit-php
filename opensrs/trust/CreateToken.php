28eb2b4c2647fb27d2add0ce226c653b615c17b5
Colin Campbell <colin@elusivelabs.com>
2015-09-17 15:23:54 -0400

    Refactor trust/CreateToken to new dataObject

--- opensrs/trust/CreateToken.php
+++ opensrs/trust/CreateToken.php
@@ -15,19 +15,24 @@ use OpenSRS\Exception;
 
 class CreateToken extends Base 
 {
-    private $_dataObject;
+    protected $action = "create_token";
+    protected $object = "trust_service";
+
     private $_formatHolder = '';
     public $resultFullRaw;
     public $resultRaw;
     public $resultFullFormatted;
     public $resultFormatted;
 
-    public function __construct($formatString, $dataObject)
+    public function __construct($formatString, $dataObject, $returnFullResponse = null)
     {
         parent::__construct();
-        $this->_dataObject = $dataObject;
+
         $this->_formatHolder = $formatString;
-        $this->_validateObject();
+
+        $this->_validateObject($datObject, $returnFullResponse);
+
+        $this->send($dataObject, $returnFullResponse);
     }
 
     public function __destruct()
@@ -38,46 +43,9 @@ class CreateToken extends Base
     // Validate the object
     private function _validateObject()
     {
-        $allPassed = true;
-
         if (!isset($this->_dataObject->data->order_id) and !isset($this->_dataObject->data->product_id)) {
             throw new Exception('oSRS Error - order_id or product_id is not defined.');
-            $allPassed = false;
         }
 
-        // Run the command
-        if ($allPassed) {
-            // Execute the command
-            $this->_processRequest();
-        } else {
-            throw new Exception('oSRS Error - Incorrect call.');
-        }
-    }
-
-    // Post validation functions
-    private function _processRequest()
-    {
-        $cmd = array(
-            'protocol' => 'XCP',
-            'action' => 'create_token',
-            'object' => 'trust_service',
-        );
-
-        if (isset($this->_dataObject->data->product_id) && $this->_dataObject->data->product_id != '') {
-            $cmd['attributes']['product_id'] = $this->_dataObject->data->product_id;
-        }
-        if (isset($this->_dataObject->data->order_id) && $this->_dataObject->data->order_id != '') {
-            $cmd['attributes']['order_id'] = $this->_dataObject->data->order_id;
-        }
-
-        $xmlCMD = $this->_opsHandler->encode($cmd);                    // Flip Array to XML
-        $XMLresult = $this->send_cmd($xmlCMD);                        // Send XML
-        $arrayResult = $this->_opsHandler->decode($XMLresult);        // Flip XML to Array
-
-        // Results
-        $this->resultFullRaw = $arrayResult;
-        $this->resultRaw = $arrayResult;
-        $this->resultFullFormatted = $this->convertArray2Formatted($this->_formatHolder, $this->resultFullRaw);
-        $this->resultFormatted = $this->convertArray2Formatted($this->_formatHolder, $this->resultRaw);
     }
 }
