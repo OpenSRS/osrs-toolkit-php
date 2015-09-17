85cdcbcf2bbd2d8204a638b8d0fcd6036c679aa8
Colin Campbell <colin@elusivelabs.com>
2015-09-17 17:02:28 -0400

    Add dataconversion for trust/ParseCSR

---
+++ tests/unit/opensrs/backwardcompatibility/dataconversion/trust/ParseCSRTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use OpenSRS\backwardcompatibility\dataconversion\trust\ParseCSR;
+
+/**
+ * @group backwardcompatibility
+ * @group dataconversion
+ * @group trust 
+ * @group BC_CancelOrder
+ */
+class BC_ParseCSR extends PHPUnit_Framework_TestCase
+{
+    protected $validSubmission = array(
+        "data" => array(
+            "product_type" => "",
+            "csr" => ""
+            ),
+        );
+
+    /**
+     * Valid conversion should complete with no
+     * exception thrown
+     *
+     * @return void
+     *
+     * @group validconversion
+     */
+    public function testValidDataConversion() {
+        $data = json_decode( json_encode ($this->validSubmission) );
+
+        $data->data->product_type = 'comodo_ev';
+        $data->data->csr = rand();
+
+        $shouldMatchNewDataObject = new \stdClass;
+        $shouldMatchNewDataObject->attributes = new \stdClass;
+        $shouldMatchNewDataObject->attributes->product_type = $data->data->product_type;
+        $shouldMatchNewDataObject->attributes->csr = $data->data->csr;
+
+        $pc = new ParseCSR;
+
+        $newDataObject = $pc->convertDataObject( $data );
+
+        $this->assertTrue( $newDataObject == $shouldMatchNewDataObject );
+    }
+}
