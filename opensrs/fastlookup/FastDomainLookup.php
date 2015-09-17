51fad841a514aa33639ed0a22f5103a23c10caf2
Colin Campbell <colin@elusivelabs.com>
2015-09-17 13:05:00 -0400

    Refactor Fastlookup to look more like domain api calls

--- opensrs/fastlookup/FastDomainLookup.php
+++ opensrs/fastlookup/FastDomainLookup.php
@@ -15,28 +15,22 @@ class FastDomainLookup extends FastLookup
     public $resultRaw;
     public $resultFullFormatted;
     public $resultFormatted;
+    public $tlds;
 
     public function __construct($formatString, $dataObject)
     {
-
-        parent::__construct();
         $this->setDataObject($formatString, $dataObject);
-        $this->_validateObject();
-    }
 
-    public function __destruct()
-    {
-        parent::__destruct();
+        $this->_validateObject($dataObject);
+
+        $this->send($dataObject, $this->tlds);
     }
 
     // Validate the object
-    private function _validateObject()
+    private function _validateObject($dataObject)
     {
         $domain = '';
-        $arraSelected = array();
-        $arraAll = array();
-        $arraCall = array();
-
+        
         // search domain must be definded
         if (!$this->hasDomain()) { 
             throw new Exception('oSRS Error - Search domain string not defined.');
@@ -45,54 +39,19 @@ class FastDomainLookup extends FastLookup
         // Grab domain name
         $domain = $this->getDomain();
 
-        // selected required
         if (!$this->hasSelected()) {
             throw new Exception('oSRS Error - Selected domains are not defined.');
         }
 
-        $arraSelected = $this->getSelected();
-
-        // all domains required
-        if (!$this->hasAllDomains()) {
+        if (!$this->hasAlldomains()) {
             throw new Exception('oSRS Error - All domain strinng not defined.');
         }
 
-        $arrAll = $this->getAllDomains();
+        $selected = $this->getSelected();
+        $this->tlds = $this->getAllDomains();
 
-        // Select non empty one
-        if (count($arraSelected) == 0) {
-            $arraCall = $arraAll;
-        } else {
-            $arraCall = $arraSelected; 
+        if (count(array_filter($selected)) >= 1) {
+            $this->tlds = $selected; 
         }
-
-        // Call function
-        $resObject = $this->_domainTLD($domain, $arraCall);
-    }
-
-    // Selected / all TLD options
-    private function _domainTLD($domain, $request)
-    {
-        $result = $this->checkDomainBunch($domain, $request);
-
-        // Results
-        $this->resultFullRaw = $result;
-        $this->resultRaw = $result;
-        $this->resultFullFormatted = $this->convertArray2Formatted($this->dataFormat, $this->resultFullRaw);
-        $this->resultFormatted = $this->convertArray2Formatted($this->dataFormat, $this->resultRaw);
-
-    }
-
-    public function convertArray2Formatted($type = '', $data = '')
-    {
-        $resultString = '';
-        if ($type == 'json') {
-            $resultString = json_encode($data);
-        }
-        if ($type == 'yaml') {
-            $resultString = Spyc::YAMLDump($data);
-        }
-
-        return $resultString;
     }
 }
