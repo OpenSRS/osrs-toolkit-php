0a547e1d64e3512597c5f9bdb2f02177a6d4f2c0
Kris Atkinson <kris@atkinson.me>
2015-09-17 12:41:07 -0400

    add getnotes dataconversion test

---
+++ tests/unit/opensrs/backwardcompatibility/dataconversion/domains/lookup/GetNotesTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use OpenSRS\backwardcompatibility\dataconversion\domains\lookup\GetNotes;
+
+/**
+ * @group backwardcompatibility
+ * @group dataconversion
+ * @group lookup
+ * @group BC_GetNotes
+ */
+class BC_GetNotesTest extends PHPUnit_Framework_TestCase
+{
+    protected $validSubmission = array(
+        "data" => array(
+            'cookie' => '',
+            'domain' => '',
+            'type' => '',
+            'page' => '',
+            'limit' => '',
+            'order_id' => '',
+            'transfer_id' => '',
+            ),
+        );
+
+    /**
+     * Valid conversion should complete with no
+     * exception thrown
+     *
+     * @return void
+     *
+     * @group validconversion
+     */
+    public function testValidDataConversion() {
+        $data = json_decode( json_encode ($this->validSubmission) );
+
+        $data->data->cookie = md5(time());
+        $data->data->domain = "phptest" . time() . ".com";
+        $data->data->type = "test";
+        $data->data->page = "10";
+        $data->data->limit = "100";
+        $data->data->order_id = time();
+        $data->data->transfer_id = strtotime("+1 week");
+
+        $shouldMatchNewDataObject = new \stdClass;
+        $shouldMatchNewDataObject->attributes = new \stdClass;
+
+        $shouldMatchNewDataObject->cookie = $data->data->cookie;
+        
+        $shouldMatchNewDataObject->attributes->domain = $data->data->domain;
+        $shouldMatchNewDataObject->attributes->type = $data->data->type;
+        $shouldMatchNewDataObject->attributes->page = $data->data->page;
+        $shouldMatchNewDataObject->attributes->limit = $data->data->limit;
+        $shouldMatchNewDataObject->attributes->order_id = $data->data->order_id;
+        $shouldMatchNewDataObject->attributes->transfer_id = $data->data->transfer_id;
+
+        $ns = new GetNotes();
+
+        $newDataObject = $ns->convertDataObject( $data );
+
+        $this->assertTrue( $newDataObject == $shouldMatchNewDataObject );
+    }
+}
