3b81b41f1286890a2a7fdd7051800a09b373b185
Colin Campbell <colin@elusivelabs.com>
2015-09-17 17:34:25 -0400

    Add dataconversion for trust/ProcessPending

---
+++ tests/unit/opensrs/backwardcompatibility/dataconversion/trust/ProcessPendingTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use OpenSRS\backwardcompatibility\dataconversion\trust\ProcessPending;
+
+/**
+ * @group backwardcompatibility
+ * @group dataconversion
+ * @group trust 
+ * @group BC_ProcessPending
+ */
+class BC_ProcessPending extends PHPUnit_Framework_TestCase
+{
+    protected $validSubmission = array(
+        "data" => array(
+            "order_id" => ""
+            ),
+        );
+
+    /**
+     * Valid conversion should complete with no
+     * exception thrown
+     *
+     * @return void
+     *
+     * @group validconversion
+     */
+    public function testValidDataConversion() {
+        $data = json_decode( json_encode ($this->validSubmission) );
+
+        $data->data->order_id = '123';
+
+        $shouldMatchNewDataObject = new \stdClass;
+        $shouldMatchNewDataObject->attributes = new \stdClass;
+        $shouldMatchNewDataObject->attributes->order_id = $data->data->order_id;
+
+        $pp = new ProcessPending;
+
+        $newDataObject = $pp->convertDataObject( $data );
+
+        $this->assertTrue( $newDataObject == $shouldMatchNewDataObject );
+    }
+}
