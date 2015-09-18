128b42159860471f6c13ac379478961dbe1cf088
Kris Atkinson <kris@atkinson.me>
2015-09-18 09:17:30 -0400

    add subuser dataconversion unit tests

---
+++ tests/unit/opensrs/backwardcompatibility/dataconversion/domains/subuser/SubuserGetInfoTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use OpenSRS\backwardcompatibility\dataconversion\domains\subuser\SubuserGetInfo;
+
+/**
+ * @group backwardcompatibility
+ * @group dataconversion
+ * @group subuser
+ * @group BC_SubuserGetInfo
+ */
+class BC_SubuserGetInfoTest extends PHPUnit_Framework_TestCase
+{
+    protected $validSubmission = array(
+        "data" => array(
+			'cookie' => '',
+
+			'domain' => '',
+            ),
+        );
+
+    /**
+     * Valid conversion should complete with no
+     * exception thrown
+     *
+     * @return void
+     *
+     * @group validconversion
+     */
+    public function testValidDataConversion() {
+        $data = json_decode( json_encode ($this->validSubmission) );
+
+        $data->data->cookie = md5(time());
+
+        $data->data->domain = "phptest" . time() . '.com';
+
+        $shouldMatchNewDataObject = new \stdClass;
+        $shouldMatchNewDataObject->attributes = new \stdClass;
+
+        $shouldMatchNewDataObject->cookie = $data->data->cookie;
+
+        $shouldMatchNewDataObject->attributes->domain = $data->data->domain;
+
+        $ns = new SubuserGetInfo();
+        $newDataObject = $ns->convertDataObject( $data );
+
+        $this->assertTrue( $newDataObject == $shouldMatchNewDataObject );
+    }
+}
