8e29469b84b0dd31d1ff6c5e0996bae213045357
Colin Campbell <colin@elusivelabs.com>
2015-09-21 14:41:26 -0400

    Update trust/QueryApproverList to new dataObject

--- opensrs/trust/QueryApproverList.php
+++ opensrs/trust/QueryApproverList.php
@@ -15,19 +15,24 @@ use OpenSRS\Exception;
 
 class QueryApproverList extends Base
 {
-    private $_dataObject;
+    protected $action = 'query_qpprover_list';
+    protected $object = 'trust_service';
+
     private $_formatHolder = '';
     public $resultFullRaw;
     public $resultRaw;
     public $resultFullFormatted;
     public $resultFormatted;
 
-    public function __construct($formatString, $dataObject)
+    public function __construct($formatString, $dataObject, $returnFullResponse = true)
     {
         parent::__construct();
-        $this->_dataObject = $dataObject;
+
         $this->_formatHolder = $formatString;
-        $this->_validateObject();
+
+        $this->_validateObject($dataObject);
+
+        $this->send($dataObject, $returnFullResponse);
     }
 
     public function __destruct()
@@ -36,10 +41,8 @@ class QueryApproverList extends Base
     }
 
     // Validate the object
-    private function _validateObject()
+    public function _validateObject($dataObject)
     {
-        $allPassed = true;
-
         if (!isset($this->_dataObject->data->product_type)) {
             throw new Exception('oSRS Error - product_type is not defined.');
             $allPassed = false;
@@ -48,37 +51,5 @@ class QueryApproverList extends Base
             throw new Exception('oSRS Error - domain is not defined.');
             $allPassed = false;
         }
-
-        // Run the command
-        if ($allPassed) {
-            // Execute the command
-            $this->_processRequest();
-        } else {
-            throw new Exception('oSRS Error - Incorrect call.');
-        }
-    }
-
-    // Post validation functions
-    private function _processRequest()
-    {
-        $cmd = array(
-            'protocol' => 'XCP',
-            'action' => 'query_approver_list',
-            'object' => 'trust_service',
-            'attributes' => array(
-                'domain' => $this->_dataObject->data->domain,
-                'product_type' => $this->_dataObject->data->product_type,
-            ),
-        );
-
-        $xmlCMD = $this->_opsHandler->encode($cmd);                    // Flip Array to XML
-        $XMLresult = $this->send_cmd($xmlCMD);                        // Send XML
-        $arrayResult = $this->_opsHandler->decode($XMLresult);        // Flip XML to Array
-
-        // Results
-        $this->resultFullRaw = $arrayResult;
-        $this->resultRaw = $arrayResult;
-        $this->resultFullFormatted = $this->convertArray2Formatted($this->_formatHolder, $this->resultFullRaw);
-        $this->resultFormatted = $this->convertArray2Formatted($this->_formatHolder, $this->resultRaw);
     }
 }
