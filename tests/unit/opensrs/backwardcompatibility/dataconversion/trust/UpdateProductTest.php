bf2b1349f0995a60a11b715a5d4b2448302162d8
Colin Campbell <colin@elusivelabs.com>
2015-09-22 16:47:03 -0400

    Add dataconversion for trust/UpdateProduct

---
+++ tests/unit/opensrs/backwardcompatibility/dataconversion/trust/UpdateProductTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use OpenSRS\backwardcompatibility\dataconversion\trust\UpdateProduct;
+
+/**
+ * @group backwardcompatibility
+ * @group dataconversion
+ * @group trust
+ * @group BC_UpdateProduct
+ */
+class BC_UpdateProductTest extends PHPUnit_Framework_TestCase
+{
+    protected $validSubmission = array(
+        "data" => array(
+            'product_id' => ''
+        ),
+    );
+
+    /**
+     * Valid conversion should complete with no
+     * exception thrown
+     *
+     * @return void
+     *
+     * @group validconversion
+     */
+    public function testValidDataConversion() {
+        $data = json_decode( json_encode ($this->validSubmission) );
+
+        $data->data->product_id = '123';
+
+        $shouldMatchNewDataObject = new \stdClass;
+        $shouldMatchNewDataObject->attributes = new \stdClass;
+
+        $shouldMatchNewDataObject->attributes->product_id = $data->data->product_id;
+
+        $pc = new UpdateProduct;
+
+        $newDataObject = $pc->convertDataObject( $data );
+
+        $this->assertTrue( $newDataObject == $shouldMatchNewDataObject );
+    }
+}
