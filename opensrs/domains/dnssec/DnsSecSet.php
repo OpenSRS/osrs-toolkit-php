aee8a3a21081b5637e252cd8c8e7ccc715d7fe7d
Andy Richdale <andy.richdale@rocketspark.com>
2022-03-31 11:53:20 +1300

    Set DNSSEC endpoint

---
+++ opensrs/domains/dnssec/DnsSecSet.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace opensrs\domains\dnssec;
+
+use opensrs\Base;
+
+class DnsSecSet extends Base
+{
+    public $action = 'set_dnssec_info';
+    public $object = 'domain';
+
+    public $_formatHolder = '';
+    public $resultFullRaw;
+    public $resultRaw;
+    public $resultFullFormatted;
+    public $resultFormatted;
+
+    public $requiredFields = array(
+        'attributes' => array(
+            'dnssec',
+        ),
+        'domain'
+    );
+
+    public function __construct($formatString, $dataObject, $returnFullResponse = true)
+    {
+        parent::__construct();
+
+        $this->_formatHolder = $formatString;
+
+        $this->_validateObject($dataObject);
+
+        $this->send($dataObject, $returnFullResponse);
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+}
